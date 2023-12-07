b34f8e29b12af79e132b31600ac65e8b2d46c4f4
Daniel Santiago Aguila Torres <daniel.santiago.aguila.torres@gmail.com>
2023-12-06 23:55:29 -0800

    feat: add distance function

--- src/main.c
+++ src/main.c
@@ -1,13 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include "mnist.c"
 
 
-float distances(float* x1, float* x2, int n_features, int n_samples) {
-  #pragma omp parallel for
-  for (int i = 0; i < n_samples && i + 8 < i + n_features; i += 8) {
+float *distances(float* x1, float* x2, int n_features, int n_samples) {
+  float *values = malloc(sizeof(float) * n_samples);
 
+  #pragma omp parallel for
+  for (int i = 0; i < n_samples; i++) {
+    values[i] = 0.0f;
+    for (int j = 0; j < n_features; j++) {
+      int n = i*n_features + j;
+      float diference = (x1[n] - x2[n]);
+      values[i] += diference * diference;
+    }
+    values[i] = sqrt(values[i]);
   }
+
+  return values;
 }
 
 int main() {
